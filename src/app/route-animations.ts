468452c727ef6f04dd338713d129f550f1588e31
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-08 22:18:51 -0400

    Refactor 'contact.component.css' to improve responsiveness and centering

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -1,166 +1,192 @@
 import {
-    trigger,
-    transition,
-    style,
-    query,
-    group,
-    animateChild,
-    animate,
-    keyframes
-  } from '@angular/animations';
-  
-  export const slideAnimation = trigger('slideAnimation', [
+  trigger,
+  transition,
+  style,
+  query,
+  group,
+  animateChild,
+  animate,
+  keyframes,
+} from '@angular/animations';
 
-    // Transitioning *to* Home from About
-    transition('About => Home', [
-      query(':enter, :leave', 
-        style({ position: 'fixed', width: '100%' }), 
-        { optional: true }),
-  
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(-100%)' }),
-          animate('1.0s ease-in-out',
-            style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-      ])
-    ]),
-  
-    // Transitioning *to* Home from Projects
-    transition('Projects => Home', [
-      query(':enter, :leave', 
-        style({ position: 'fixed', width: '100%' }), 
-        { optional: true }),
-  
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(-100%)' }),
-          animate('1.0s ease-in-out',
-            style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-      ])
-    ]),
+export const slideAnimation = trigger('slideAnimation', [
+  // Transitioning *to* Home from About
+  transition('About => Home', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
 
-    // Transitioning *to* Home from Contact
-transition('Contact => Home', [
-    query(':enter, :leave', 
-      style({ position: 'fixed', width: '100%' }), 
-      { optional: true }),
-  
     group([
-      query(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('1.0s ease-in-out',
-          style({ transform: 'translateX(0%)' }))
-      ], { optional: true }),
-    ])
-  ]),
-  
-    // Transitioning *to* About from Home
-    transition('Home => About', [
-      query(':enter, :leave', 
-        style({ position: 'fixed', width: '100%' }), 
-        { optional: true }),
-  
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(100%)' }),
-          animate('1.0s ease-in-out',
-          style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-      ])
-    ]),
-  
-    // Transitioning *to* About from Projects
-    transition('Projects => About', [
-      query(':enter, :leave', 
-        style({ position: 'fixed', width: '100%' }), 
-        { optional: true }),
-  
-      group([
-        query(':enter', [
+      query(
+        ':enter',
+        [
           style({ transform: 'translateX(-100%)' }),
-          animate('1.0s ease-in-out',
-          style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-      ])
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
     ]),
-
-    // Transitioning *to* About from Contact
-transition('Contact => About', [
-    query(':enter, :leave', 
-      style({ position: 'fixed', width: '100%' }), 
-      { optional: true }),
-  
-    group([
-      query(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('1.0s ease-in-out',
-          style({ transform: 'translateX(0%)' }))
-      ], { optional: true }),
-    ])
   ]),
-  
-    // Transitioning *to* Projects from Home
-    transition('Home => Projects', [
-      query(':enter, :leave', 
-        style({ position: 'fixed', width: '100%' }), 
-        { optional: true }),
-  
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(100%)' }),
-          animate('1.0s ease-in-out',
-            style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-      ])
-    ]),
-  
-    // Transitioning *to* Projects from About
-    transition('About => Projects', [
-      query(':enter, :leave', 
-        style({ position: 'fixed', width: '100%' }), 
-        { optional: true }),
-  
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(100%)' }),
-          animate('1.0s ease-in-out',
-            style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-      ])
-    ]),
 
-    // Transitioning *to* Projects from Contact
-transition('Contact => Projects', [
-    query(':enter, :leave', 
-      style({ position: 'fixed', width: '100%' }), 
-      { optional: true }),
-  
+  // Transitioning *to* Home from Projects
+  transition('Projects => Home', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
     group([
-      query(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('1.0s ease-in-out',
-          style({ transform: 'translateX(0%)' }))
-      ], { optional: true }),
-    ])
-  ]),
-  
-  
-    // Transitioning *to* Contact from any route
-    transition('* => Contact', [
-      query(':enter, :leave', 
-        style({ position: 'fixed', width: '100%' }), 
-        { optional: true }),
-  
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(100%)' }),
-          animate('1.0s ease-in-out',
-            style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-      ])
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(-100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
     ]),
-  
-  ]);
-  
+  ]),
+
+  // Transitioning *to* Home from Contact
+  transition('Contact => Home', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(-100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+
+  // Transitioning *to* About from Home
+  transition('Home => About', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+
+  // Transitioning *to* About from Projects
+  transition('Projects => About', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(-100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+
+  // Transitioning *to* About from Contact
+  transition('Contact => About', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(-100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+
+  // Transitioning *to* Projects from Home
+  transition('Home => Projects', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+
+  // Transitioning *to* Projects from About
+  transition('About => Projects', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+
+  // Transitioning *to* Projects from Contact
+  transition('Contact => Projects', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(-100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+
+  // Transitioning *to* Contact from any route
+  transition('* => Contact', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+      optional: true,
+    }),
+
+    group([
+      query(
+        ':enter',
+        [
+          style({ transform: 'translateX(100%)' }),
+          animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+        ],
+        { optional: true }
+      ),
+    ]),
+  ]),
+]);
